6ede76b3b0c438d7b350580bab6e9ae561b86873
AkshataJPai0505 <HP@DESKTOP-24RC1IS.(none)>
2021-01-14 19:35:37 +0530

    A new Program

---
+++ src/JavaBasics/sample6.java
@@ -0,0 +1,19 @@
+package JavaBasics;
+
+public class sample6 {
+	
+	public static void main(String args[]) {
+		sample6 s=new sample6();
+		System.out.println("This is main");
+		
+	}
+	static
+	{
+		System.out.println("This is Static");
+	}
+	{
+		System.out.println("This is Non Static");
+	}
+	
+
+}
